fcc1aec164a51ae0f62f815dfea1fe620e1aa93f
Sebastián Valdés <svaldes@modyo.com>
2019-03-20 11:11:02 -0300

    update

--- src/main/java/com/modyo/desa/demojpa/DemoJpaApplication.java
+++ src/main/java/com/modyo/desa/demojpa/DemoJpaApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoJpaApplication {
 
+	private String noSeUsa;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJpaApplication.class, args);
 	}
